715adc9125163f115f342f144697c50245548fa7
Anamikamahi18 <anamikamahi18@gmail.com>
2026-06-23 16:27:51 +0530

    Ready for Backend Deployment

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.api.heatwave import (
     router as heatwave_router
@@ -31,13 +31,13 @@ app = FastAPI(
 )
 
 # Enable CORS
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(
